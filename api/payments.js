be618ba6d041b44b9334e7271d48147c7064a964
edodge <edodge11@gmail.com>
2015-08-05 14:23:42 -0400

    SQL query profiling

--- api/payments.js
+++ api/payments.js
@@ -22,11 +22,17 @@ app.get('/', function(req, res) {
 	        console.error("An error occurred: " + err);
 	    }
 
+		// var pre_query = new Date().getTime();
 	    connection.query("SELECT state_code,state_name,total_transactions tot_trn,date,ROUND(mrc_mre_mean,2) mrc_mre,ROUND(mre_wlg_mean,2) mre_wlg,ROUND(wlg_wls_mean,2) wlg_wls,ROUND(wls_fto_mean,2) wls_fto,ROUND(fto_firstsign_mean,2) fto_sn1,ROUND(firstsign_secondsign_mean,2) sn1_sn2,ROUND(secondsign_processed_mean,2) sn2_prc FROM state_delays_duration a WHERE a.gender = 'both' AND a.bank_type = 'b' AND a.date_type = 'processed_date' AND a.total_transactions > 0 AND a.state_code in (SELECT b.state_code FROM blocks b WHERE b.block_code ='"+BLOCK_CODE+"') ORDER BY date ASC; SELECT district_code,district_name,total_transactions tot_trn,date,ROUND(mrc_mre_mean,2) mrc_mre,ROUND(mre_wlg_mean,2) mre_wlg,ROUND(wlg_wls_mean,2) wlg_wls,ROUND(wls_fto_mean,2) wls_fto,ROUND(fto_firstsign_mean,2) fto_sn1,ROUND(firstsign_secondsign_mean,2) sn1_sn2,ROUND(secondsign_processed_mean,2) sn2_prc FROM district_delays_duration a WHERE a.gender = 'both' AND a.bank_type = 'b' AND a.date_type = 'processed_date' AND a.total_transactions > 0 AND a.district_code in (SELECT b.district_code FROM blocks b WHERE b.block_code ='"+BLOCK_CODE+"') ORDER BY date ASC; SELECT block_code,block_name,total_transactions tot_trn,date,ROUND(mrc_mre_mean,2) mrc_mre,ROUND(mre_wlg_mean,2) mre_wlg,ROUND(wlg_wls_mean,2) wlg_wls,ROUND(wls_fto_mean,2) wls_fto,ROUND(fto_firstsign_mean,2) fto_sn1,ROUND(firstsign_secondsign_mean,2) sn1_sn2,ROUND(secondsign_processed_mean,2) sn2_prc FROM block_delays_duration WHERE gender = 'both' AND bank_type = 'b' AND date_type = 'processed_date' AND total_transactions > 0 AND block_code ='"+BLOCK_CODE+"' ORDER BY date ASC; SELECT panchayat_code,panchayat_name,total_transactions tot_trn,date,ROUND(mrc_mre_mean,2) mrc_mre,ROUND(mre_wlg_mean,2) mre_wlg,ROUND(wlg_wls_mean,2) wlg_wls,ROUND(wls_fto_mean,2) wls_fto,ROUND(fto_firstsign_mean,2) fto_sn1,ROUND(firstsign_secondsign_mean,2) sn1_sn2,ROUND(secondsign_processed_mean,2) sn2_prc FROM panchayat_delays_duration WHERE gender = 'both' AND bank_type = 'b' AND date_type = 'processed_date' AND total_transactions > 0 AND block_code ='"+BLOCK_CODE+"' ORDER BY panchayat_code ASC, date ASC;", function(err, rows) {
 	        if (err) {
 	            throw err;
 	        } else {
 
+	   //      	// get a timestamp after running the query
+				// var post_query = new Date().getTime();
+				// // calculate the duration in seconds
+				// var duration = (post_query - pre_query) / 1000;
+				// console.log(duration);
 	            var final_dict = {};
 
 	            var stateResponse = rows[0];
