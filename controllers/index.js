5130de7932a5d95c5f1ad3613b524697e977b35a
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-19 04:30:03 +0530

    Adding express view engine

--- controllers/index.js
+++ controllers/index.js
@@ -8,9 +8,9 @@ module.exports = function (router) {
     var model = new IndexModel();
 
     router.get('/', function (req, res) {
-        
+
         res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
-        
+
     });
 
 };
