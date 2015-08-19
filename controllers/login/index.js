e7bed2c724a1284ac9df988836a79034d6f3426c
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-19 07:29:09 +0530

    Addign login form

---
+++ controllers/login/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+var LoginModel = require('../../models/login'),
+  passport = require('passport');
+
+
+module.exports = function(router) {
+
+  var model = new LoginModel();
+
+  router.get('/', function(req, res) {
+    //Include any error messages that come from the login process.
+    model.messages = req.flash('error');
+    res.render('login', model);
+  });
+
+  /**
+   * Receive the login credentials and authenticate.
+   * Successful authentications will go to /profile or if the user was trying to access a secured resource, the URL
+   * that was originally requested.
+   *
+   * Failed authentications will go back to the login page with a helpful error message to be displayed.
+   */
+  router.post('/', function(req, res) {
+
+    passport.authenticate('local', {
+      successRedirect: req.session.goingTo || '/profile',
+      failureRedirect: '/login',
+      failureFlash: true
+    })(req, res);
+
+  });
+
+};
