03f191cb0c99a4cc7f47b946bae2c56505499378
Ravi Suhag <suhag.ravi@gmail.com>
2015-09-03 14:18:36 +0530

    finihsing edit profile feature

--- controllers/profile/edit.js
+++ controllers/profile/edit.js
@@ -1,19 +1,47 @@
 'use strict';
-
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var _ = require('lodash');
 
 module.exports = function(router) {
 
   router.get('/', function(req, res) {
     if (req.isAuthenticated()) {
-      res.redirect('/profile');
-    } else {
       res.render('edit-profile');
+    } else {
+      res.redirect('/login');
     }
 
   });
 
   router.post('/', function(req, res) {
 
+    var user = req.user;
+    delete req.body.role;
+
+    if (user) {
+      user = _.extend(user, req.body);
+      user.updated = Date.now();
+      user.save(function(err) {
+        if (err) {
+          return res.status(400).send({
+            message: 'err'
+          });
+        } else {
+          req.login(user, function(err) {
+            if (err) {
+              res.status(400).send(err);
+            } else {
+               res.render('edit-profile');
+            }
+          });
+        }
+      });
+    } else {
+      res.status(400).send({
+        message: 'User is not signed in'
+      });
+    }
 
 
   });
