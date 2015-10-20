bdd2561b05646c2859b6f5c36cec56a6dccd5166
Ravi Suhag <suhag.ravi@gmail.com>
2015-10-20 16:43:43 +0530

    Applying password encryption for SQL users

--- app/controllers/auth/login.js
+++ app/controllers/auth/login.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose');
 var Boom = require('boom');
 var Joi = require('joi');
+var crypto = require('crypto');
 
 var lockoutInterval = 60; // seconds
 var maxAttemptsBeforeLockout = 5;
@@ -29,55 +30,56 @@ exports.showForm = {
     }
 };
 
-// exports.postForm = {
-//     description: 'Post to the login page',
-//     auth: {
-//         mode: 'try',
-//         strategy: 'standard'
-//     },
-//     plugins: {
-//         'hapi-auth-cookie': {
-//             redirectTo: false
-//         },
-//         crumb: {
-//             key: 'crumb',
-//             source: 'payload', // this tests payload crumb value.
-//             restful: true // do not need to make Joi validation for crumb.
-//         }
-//     },
-//     validate: {
-//         payload: { // payload for POST, query for GET
-//             username: Joi.string().min(3).max(20),
-//             password: Joi.string().min(6).max(20)
-//         },
-//         failAction: function(request, reply, source, error) {
-//             // Username, passowrd minimum validation failed
-//             request.session.flash('error', 'Invalid username or password');
-//             return reply.redirect('/login');
-//         },
-//     },
-//     handler: function(request, reply) {
-//         if (request.auth.isAuthenticated) {
-//             return reply.redirect('/account');
-//         }
-//         User.findByCredentials(request.payload.username, request.payload.password, function(err, user, msg) {
-//             var context = {};
-//             if (err) {
-//                 // Boom bad implementation
-//                 request.session.flash('error', 'An internal server error occurred');
-//                 return reply.redirect('/login');
-//             }
-//             if (user) {
-//                 // TO DO : Check having before session set is good or bad
-//                 request.auth.session.set(user);
-//                 return reply.redirect('/account');
-//             } else {
-//                 // User not fond in database
-//                 request.session.flash('error', 'Invalid username or password');
-//                 return reply.redirect('/login');
-//             }
-//         });
+exports.postForm = {
+    description: 'Post to the login page',
+    auth: {
+        mode: 'try',
+        strategy: 'standard'
+    },
+    plugins: {
+        'hapi-auth-cookie': {
+            redirectTo: false
+        },
+        crumb: {
+            key: 'crumb',
+            source: 'payload', // this tests payload crumb value.
+            restful: true // do not need to make Joi validation for crumb.
+        }
+    },
+    validate: {
+        payload: { // payload for POST, query for GET
+            username: Joi.string().min(3).max(20),
+            password: Joi.string().min(6).max(20)
+        },
+        failAction: function(request, reply, source, error) {
+            // Username, passowrd minimum validation failed
+            request.session.flash('error', 'Invalid username or password');
+            return reply.redirect('/login');
+        },
+    },
+    handler: function(request, reply) {
+        if (request.auth.isAuthenticated) {
+            return reply.redirect('/account');
+        }
+        var User = request.server.plugins.sequelize.db.User;
+        // sySZ7k7Vef+odY4vQ9hRbAB0E0VW//3KEDjwqYvBzjG0uLMewJ+CmFkUwlk/hjNGmaRCczh3Zfv+9I9a778Xww==
+        // console.log(crypto.createHash('md5').update(request.payload.password).digest('hex'));
+        // e10adc3949ba59abbe56e057f20f883e
+        User.findOne({
+            where: {
+                username: request.payload.username,
+                password: crypto.createHash('md5').update(request.payload.password).digest('hex')
+            }
+        }).then(function(user) {
+            if (user) {
+                request.auth.session.set(user);
+                return reply.redirect('/account');
+            } else {
+                // User not fond in database
+                request.session.flash('error', 'Invalid username or password');
+                return reply.redirect('/login');
+            }
+        });
 
-
-//     }
-// };
+    }
+};
