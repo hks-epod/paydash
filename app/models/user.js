872bb8cbf8b6c8f651193918149bab5dd35a1d92
Ravi Suhag <suhag.ravi@gmail.com>
2015-10-20 13:53:41 +0530

    Adding user schema for mysql

--- app/models/user.js
+++ app/models/user.js
@@ -1,18 +1,33 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
+    var User = sequelize.define('User', {
+        firstname: DataTypes.STRING,
+        lastname: DataTypes.STRING,
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        role: DataTypes.STRING,
+        gender: DataTypes.STRING,
+        dob: DataTypes.STRING,
+        mobile: DataTypes.STRING,
+        email: DataTypes.STRING,
+        sas: DataTypes.BOOLEAN,
+        sas_years: DataTypes.STRING,
+        ias: DataTypes.BOOLEAN,
+        ias_years: DataTypes.STRING,
+        title: DataTypes.STRING,
+        region_type: DataTypes.STRING,
+        region_name: DataTypes.STRING,
+        work_email: DataTypes.STRING,
+        work_years: DataTypes.STRING,
+        time_on_nrega: DataTypes.STRING,
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
 
-  return User;
+    return User;
 };
