d54afe0d44c8be68727eff50f390d0182e5e66a2
edodge <edodge11@gmail.com>
2015-10-22 15:40:40 -0400

    Separating block and panchayat API's

--- app/controllers/dashboard/performance-block.js
+++ app/controllers/dashboard/performance-block.js
@@ -1,4 +1,5 @@
 'use strict';
+var d3 = require('d3');
 
 var queries = require('../../helpers/queries');
 var dummy = require('../../helpers/dummy');
@@ -16,14 +17,95 @@ exports.getData = {
         var block_code = '1709003';
         var queryString = queries.blockPerformance(block_code);
 
-        // sequelize.query('SELECT * from users', {
-        //     type: sequelize.QueryTypes.SELECT
-        // }).then(function(results) {
-        //     reply(results);
-        // });
+        sequelize.query(queryString, {
+            type: sequelize.QueryTypes.SELECT
+        }).then(function(rows) {
+
+            var final_dict = {};
+            var stateResponse = rows[0];
+            var districtResponse = rows[1];
+            var blockResponse = rows[2];
+
+            // process state data
+            var stateName = stateResponse[0].state_name;
+            var stateCode = stateResponse[0].state_code;
+            final_dict.state = {
+                'state_code': stateCode,
+                'state_name': stateName,
+                'data': stateResponse.map(function(d) {
+                    return [
+                        d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
+                        d.mrc_mre,
+                        d.mre_wlg,
+                        d.wlg_wls,
+                        d.wls_fto,
+                        d.fto_sn1,
+                        d.sn1_sn2,
+                        d.sn2_prc,
+                        d.tot_trn
+                    ];
+                })
+            };
+
+            // process district data
+            var districtName = districtResponse[0].district_name;
+            var districtCode = districtResponse[0].district_code;
+            final_dict.district = {
+                'district_code': districtCode,
+                'district_name': districtName,
+                'data': districtResponse.map(function(d) {
+                    return [
+                        d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
+                        d.mrc_mre,
+                        d.mre_wlg,
+                        d.wlg_wls,
+                        d.wls_fto,
+                        d.fto_sn1,
+                        d.sn1_sn2,
+                        d.sn2_prc,
+                        d.tot_trn
+                    ];
+                })
+            };
+
+            // process block data
+            var blockName = blockResponse[0].block_name;
+            var blockCode = blockResponse[0].block_code;
+            final_dict.block = {
+                'block_code': blockCode,
+                'block_name': blockName,
+                'data': blockResponse.map(function(d) {
+                    return [
+                        d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
+                        d.mrc_mre,
+                        d.mre_wlg,
+                        d.wlg_wls,
+                        d.wls_fto,
+                        d.fto_sn1,
+                        d.sn1_sn2,
+                        d.sn2_prc,
+                        d.tot_trn
+                    ];
+                })
+            };
+
+            var headers = ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'];
+            final_dict.block_name = blockName;
+            final_dict.config = {
+                'headers': headers,
+                'mandated_days': {}
+            };
+
+            function padNum(num) {
+                var str = num.toString();
+                return str.length === 1 ? '0' + str : str;
+            }
+            
+            reply(final_dict);
+        });
         
 
-        reply(dummy.blockData); 
+        // reply(dummy.blockData); 
         
 
     }
--- app/controllers/dashboard/performance-panchayat.js
+++ app/controllers/dashboard/performance-panchayat.js
@@ -1,4 +1,5 @@
 'use strict';
+var d3 = require('d3');
 
 var queries = require('../../helpers/queries');
 var dummy = require('../../helpers/dummy');
@@ -22,14 +23,98 @@ exports.getData = {
         var block_code = '1709003';
         var queryString = queries.blockPerformance(block_code);
 
-        // sequelize.query('SELECT * from users', {
-        //     type: sequelize.QueryTypes.SELECT
-        // }).then(function(results) {
-        //     reply(results);
-        // });
+        sequelize.query(queryString, {
+            type: sequelize.QueryTypes.SELECT
+        }).then(function(rows) {
+            
+            var final_dict = {};
+            var blockName = rows[0][0].block_name;
+            var panchayatResponse = rows[1];
+            var employeeResponse = rows[2];
+            
+            // process panchayat data
+            final_dict.panchayats = d3.nest()
+                .key(function(d) {
+                    return d.panchayat_code;
+                })
+                .rollup(function(v) {
+                    return {
+                        'panchayat_code': v[0].panchayat_code,
+                        'panchayat_name': v[0].panchayat_name,
+                        'data': v.map(function(d) {
+                            return [
+                                d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
+                                d.mrc_mre,
+                                d.mre_wlg,
+                                d.wlg_wls,
+                                d.wls_fto,
+                                d.fto_sn1,
+                                d.sn1_sn2,
+                                d.sn2_prc,
+                                d.tot_trn
+                            ];
+                        })
+                    };
+                })
+                .entries(panchayatResponse)
+                .map(function(d) {
+                    return d.values;
+                })
+                .sort(function(a, b) {
+                    var aTarget = a.data[a.data.length - 1];
+                    var bTarget = b.data[b.data.length - 1];
+                    var aSum = aTarget[1] + aTarget[2] + aTarget[3] + aTarget[4] + aTarget[5] + aTarget[6] + aTarget[7];
+                    var bSum = bTarget[1] + bTarget[2] + bTarget[3] + bTarget[4] + bTarget[5] + bTarget[6] + bTarget[7];
+                    return bSum - aSum;
+                });
+
+            // process employee data
+            final_dict.employees = d3.nest()
+                .key(function(d) {
+                    return d.designation;
+                })
+                .rollup(function(v) {
+                    return d3.nest()
+                        .key(function(d) {
+                            return d.staff_id;
+                        })
+                        .rollup(function(w) {
+                            return {
+                                'staff_id': w[0].staff_id,
+                                'name': w[0].name,
+                                'mobile': w[0].mobile_no,
+                                'panchayats': w.map(function(d) {
+                                    return {
+                                        'panchayat_code': d.map_location,
+                                        'panchayat_name': d.mapped_panchayat_name
+                                    };
+                                })
+                            };
+                        })
+                        .entries(v)
+                        .map(function(d) {
+                            return d.values;
+                        });
+
+                })
+                .map(employeeResponse);
+
+            var headers = ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'];
+            final_dict.block_name = blockName;
+            final_dict.config = {
+                'headers': headers,
+            };
+
+            function padNum(num) {
+                var str = num.toString();
+                return str.length === 1 ? '0' + str : str;
+            }
+            
+            reply(final_dict);
+        });
 
 
-        reply(dummy.blockData);
+        // reply(dummy.blockData);
 
 
     }
