a4f17f13a49ca405d94e49691b4a538721644403
Ravi Suhag <suhag.ravi@gmail.com>
2015-10-31 04:46:07 +0530

    Adding pending musters filteration

--- app/controllers/musters/delayed-musters.js
+++ app/controllers/musters/delayed-musters.js
@@ -5,6 +5,7 @@ var queries = require('../../helpers/queries');
 var musters_urls = require('../../helpers/musters');
 var req = require('request');
 var _ = require('lodash');
+var moment = require('moment');
 
 exports.showPage = {
     handler: function(request, reply) {
@@ -23,7 +24,30 @@ exports.getData = {
 
             req(urls.delayed_musters, function(error, response, body) {
                 if (!error && response.statusCode === 200) {
-                    reply(JSON.parse(body));
+
+                    body = JSON.parse(body);
+                    var timInt = [2, 5, 6, 7, 8];
+                    var pending_musters = {};
+
+                    timInt.forEach(function(val, index) {
+
+                        pending_musters['ds_t' + val] = body['ds_t' + val].filter(function(muster) {
+
+                            var start_date = moment(muster.start_date, 'DD/MM/YYYY');
+                            var today = moment().startOf('day').subtract(val,'d');
+                            return start_date.toDate() < today.toDate();
+
+                        }).map(function(muster) {
+                            muster.end_date = muster.End_date;
+                            delete muster.End_date;
+                            return muster;
+                        });
+                      
+                    });
+
+                    reply(pending_musters);
+
+
                 }
             });
         });
