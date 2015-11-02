a8abeaa1e6a986e887dc1fd61cf922a39935088a
edodge <edodge11@gmail.com>
2015-11-02 11:14:39 -0500

    Fixing typo

--- app/helpers/queries.js
+++ app/helpers/queries.js
@@ -10,11 +10,11 @@ exports.blockPerformance = function(BLOCK_CODE) {
 exports.panchayatPerformance = function(BLOCK_CODE) {
     return "SELECT block_name FROM blocks WHERE block_code = '" + BLOCK_CODE + "';" +
     	"SELECT panchayat_code,panchayat_name,total_transactions tot_trn,date,ROUND(mrc_mre_mean,2) mrc_mre,ROUND(mre_wlg_mean,2) mre_wlg,ROUND(wlg_wls_mean,2) wlg_wls,ROUND(wls_fto_mean,2) wls_fto,ROUND(fto_firstsign_mean,2) fto_sn1,ROUND(firstsign_secondsign_mean,2) sn1_sn2,ROUND(secondsign_processed_mean,2) sn2_prc FROM panchayat_delays_duration WHERE gender = 'both' AND bank_type = 'all' AND date_type = 'processed_date' AND total_transactions > 0 AND block_code ='" + BLOCK_CODE + "' ORDER BY panchayat_code ASC, date ASC;" +
-        "SELECT staff_id, name, task_assign, mobile_no, map_location, mapped_panchayat_name FROM employees WHERE block_code = '" + BLOCK_CODE + "';";
+        "SELECT staff_id, name, task_assign, mobile_no, map_location, mapped_panchayat_name FROM employees WHERE task_assign IN ('TA','GRS') AND block_code = '" + BLOCK_CODE + "';";
 };
 
 exports.apiHelper = function(BLOCK_CODE) {
     return "SELECT short_name, state_code FROM short_codes a WHERE a.state_code in (SELECT b.state_code FROM blocks b WHERE b.block_code ='" + BLOCK_CODE + "');" +
-    " SELECT  name,task_assign, designation, mobile_no, map_location, mapped_panchayat_name FROM employees WHERE  task_assign IN ('TA','GRS') AND block_code ='" + BLOCK_CODE + "';";
+    "SELECT name, task_assign, mobile_no, map_location, mapped_panchayat_name FROM employees WHERE task_assign IN ('TA','GRS') AND block_code ='" + BLOCK_CODE + "';";
 };
 
