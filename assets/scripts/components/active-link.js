038cb53cb10f6fb38d64eec2fe5e8d89a63e7b1b
Ravi Suhag <suhag.ravi@gmail.com>
2015-11-21 10:13:38 +0530

    responsive menu

--- assets/scripts/components/active-link.js
+++ assets/scripts/components/active-link.js
@@ -1,5 +1,15 @@
 'use strict';
 var $ = require('jquery');
+var d3 = require('d3');
 
-$('a[href="'+ location.pathname + '"]').addClass('active');
+$('a[href="' + location.pathname + '"]').addClass('active');
 
+// MENU
+d3.selectAll('#top-ham').on('click', function() {
+    d3.selectAll('#top-ham').classed('active', function(d, i) {
+        return !d3.select(this).classed('active');
+    });
+    d3.selectAll('.menu-wrapper').classed('active', function(d, i) {
+        return !d3.select(this).classed('active');
+    });
+});
