0b85e3922304980453d6d4a6c5d665e0fbe182e9
Ravi Suhag <suhag.ravi@gmail.com>
2016-01-15 14:15:18 +0530

    Add heap and mem usage charts

--- assets/scripts/monitor/server.js
+++ assets/scripts/monitor/server.js
@@ -8,13 +8,34 @@ var serverMonitor = {};
 
 function loadLineCharts() {
     var cpu_usage_data = parser.lines(serverMonitor.data, 'cpu');
-    console.log(cpu_usage_data);
     chart.ga_chart({
         data: cpu_usage_data,
         title: 'CPU Usages',
         target: '#cpu_usage_chart',
         legend_target: '.block_legend',
         labels: ['CPU  usage'],
+        legend_labels: 'labels',
+        area: true
+    });
+
+    var mem_usage_data = parser.lines(serverMonitor.data, 'mem_used');
+    chart.ga_chart({
+        data: mem_usage_data,
+        title: 'Memory Usages',
+        target: '#mem_usage_chart',
+        legend_target: '.block_legend',
+        labels: ['Memory  usage'],
+        legend_labels: 'labels',
+        area: true
+    });
+
+    var heap_usage_data = parser.lines(serverMonitor.data, 'heap_used');
+    chart.ga_chart({
+        data: heap_usage_data,
+        title: 'Heap Usages',
+        target: '#heap_usage_chart',
+        legend_target: '.block_legend',
+        labels: ['Heap  usage'],
         legend_labels: 'labels',
         area: true
     });
@@ -29,6 +50,7 @@ function serverMonInit() {
         })
         .get(function(error, data) {
             serverMonitor.data = data;
+            console.log('sdsdsdsd');
             loadLineCharts();
         });
 }
