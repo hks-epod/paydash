a958146aa6043ffc7d6f75701ee18b74fc799804
Ravi Suhag <suhag.ravi@gmail.com>
2016-01-20 21:06:33 +0530

    add lib test

---
+++ test/lib/sequelize.js
@@ -0,0 +1,26 @@
+// 'use strict';
+
+// var Lab = require('lab');
+// var Code = require('code');
+// var Hapi = require('hapi');
+// var sequelize = require('../../lib/sequelize');
+
+// var lab = exports.lab = Lab.script();
+
+// lab.experiment('Sequelize plugin', function() {
+
+//     var server = new Hapi.Server();
+//     server.connection();
+
+//     lab.test('Plugins successfully loads', function(done) {
+        
+//         server.register(sequelize, function(err) {
+
+//             Code.expect(err).to.not.exist();
+//             done();
+            
+//         });
+
+//     });
+
+// });
