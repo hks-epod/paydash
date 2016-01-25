c415f6573f3f4444d5565bcfc709781b0478c710
Ravi Suhag <suhag.ravi@gmail.com>
2016-01-25 16:13:06 +0530

    Add treatment arms to charts

--- app/controllers/monitor/analysis.js
+++ app/controllers/monitor/analysis.js
@@ -24,12 +24,17 @@ exports.getData = {
             var outcomes = data[0];
             var treatments = data[1];
             var summary = data[2];
+            var arms = data[3];
+            var arm_summary = data[4];
 
             for (var index in outcomes) {
 
                 var result = {
                     treatment: [],
-                    control: []
+                    control: [],
+                    arm_1: [],
+                    arm_2: [],
+                    arm_3: []
                 };
                 result.label = outcomes[index].label;
                 result.name = outcomes[index].outcome;
@@ -39,13 +44,29 @@ exports.getData = {
                         result.treatment.push(treatments[treatment_index]);
                     }
                     if (treatments[treatment_index].outcome === outcomes[index].outcome && treatments[treatment_index].treatment === 0) {
-
                         result.control.push(treatments[treatment_index]);
+                    }
+                }
+                for (var arm_index in arms) {
+                    arms[arm_index].value = arms[arm_index].mean;
+                    if (arms[arm_index].outcome === outcomes[index].outcome && arms[arm_index].arm === 1) {
+                        result.arm_1.push(arms[arm_index]);
+                    }
+                    if (arms[arm_index].outcome === outcomes[index].outcome && arms[arm_index].arm === 2) {
+                        result.arm_2.push(arms[arm_index]);
+                    }
+                    if (arms[arm_index].outcome === outcomes[index].outcome && arms[arm_index].arm === 3) {
+                        result.arm_3.push(arms[arm_index]);
                     }
                 }
                 for (var stat_index in summary) {
                     if (summary[stat_index].outcome === outcomes[index].outcome) {
                         result.summary = summary[stat_index];
+                    }
+                }
+                for (var arm_stat_index in arm_summary) {
+                    if (arm_summary[arm_stat_index].outcome === outcomes[index].outcome) {
+                        result.arm_summary = arm_summary[arm_stat_index];
                     }
                 }
                 results.push(result);
--- assets/scripts/monitor/analysis.js
+++ assets/scripts/monitor/analysis.js
@@ -16,7 +16,7 @@ function outcome_chartTemplate(data) {
         .enter().append('div')
         .classed(' pure-u pure-u-24-24', true)
         .html(function(d, index) {
-            return '<h2>'+ d.label +'</h2>'+ 
+            return '<h2>' + d.label + '</h2>' +
                 '<div class="chart-holder small_chart">' +
                 '<div id="o_' + d.name + '"></div>' +
                 '<div class="p_' + d.name + '_legend"></div>' +
@@ -24,27 +24,40 @@ function outcome_chartTemplate(data) {
         });
 }
 
-function loadOutcomeCharts(data) {
+function mapDate(data, accessor) {
+    data = data.map(function(d) {
+        d[accessor] = new Date(d[accessor]);
+        return d;
+    });
+    return data;
+}
 
-    function mapDate(data, accessor) {
+function loadOutcomeCharts(data, type) {
 
-        data = data.map(function(d) {
-            d[accessor] = new Date(d[accessor]);
-            return d;
+    if (type === 'treatment') {
+        data.forEach(function(outcome) {
+
+            chart.outcome_chart({
+                data: [outcome.treatment, outcome.control],
+                title: outcome.label,
+                target: '#o_' + outcome.name,
+                labels: ['Treatment', 'Control']
+            });
         });
-        return data;
+    }
+    if (type === 'arms') {
+        data.forEach(function(outcome) {
+
+            chart.outcome_chart({
+                data: [outcome.arm_1, outcome.arm_2, outcome.arm_3],
+                title: outcome.label,
+                target: '#o_' + outcome.name,
+                labels: ['Arm 1', 'Arm 2', 'Arm 3']
+            });
+        });
     }
 
-    data.forEach(function(outcome) {
-        outcome.treatment = mapDate(outcome.treatment, 'date');
-        outcome.control = mapDate(outcome.control, 'date');
-        chart.outcome_chart({
-            data: [outcome.treatment, outcome.control],
-            title: outcome.label,
-            target: '#o_' + outcome.name,
-            labels: ['Treatment', 'Control']
-        });
-    });
+
 }
 
 
@@ -55,13 +68,17 @@ function analysisMonInit() {
             console.info('progress', d3.event.loaded);
         })
         .get(function(error, data) {
+            data.forEach(function(outcome){
+                outcome.treatment = mapDate(outcome.treatment, 'date');
+                outcome.control = mapDate(outcome.control, 'date');
+                outcome.arm_1 = mapDate(outcome.arm_1, 'date');
+                outcome.arm_2 = mapDate(outcome.arm_2, 'date');
+                outcome.arm_3 = mapDate(outcome.arm_3, 'date');
+            });
 
             analysisMonitor.data = data;
             outcome_chartTemplate(data);
-
-            loadOutcomeCharts(data);
-
-
+            loadOutcomeCharts(data, 'treatment');
         });
 }
 
@@ -69,3 +86,10 @@ if (window.location.pathname === '/monitor/analysis') {
     // Laod User monitoring dashboard
     analysisMonInit();
 }
+
+// Time period Selection
+d3.selectAll('#modify-outcome-controls').on('change', function() {
+    var type = d3.event.target.value;
+    loadOutcomeCharts(analysisMonitor.data, type);
+
+});
