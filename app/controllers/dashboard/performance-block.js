23000d4c25238f1f80bcb535dd77e25f99110877
edodge <edodge11@gmail.com>
2016-02-10 18:40:00 -0500

    Generalizing API response keys; Adding logic to queries and API processing code to deal with district and block roles; Removing apiHelper function.

--- app/controllers/dashboard/performance-block.js
+++ app/controllers/dashboard/performance-block.js
@@ -14,19 +14,19 @@ exports.getData = {
     handler: function(request, reply) {
 
         var sequelize = request.server.plugins.sequelize.db.sequelize;
-        var block_code = request.query.selected_block_id;
-        var queryString = queries.blockPerformance(block_code);
+        var region_code = request.query.selected_block_id; // Ravi: update the region code location here
+        var role = 'block'; // Ravi: update the role location here
+
+        var queryString = queries.blockPerformance(region_code,role); // Ravi: update the blockPerformance function name here
 
         sequelize.query(queryString, {
             type: sequelize.QueryTypes.SELECT
         }).then(function(rows) {
 
             var final_dict = {};
-            var stateResponse = flatten(rows[0]);
-            var districtResponse = flatten(rows[1]);
-            var blockResponse = flatten(rows[2]);
 
             // process state data
+            var stateResponse = flatten(rows[0]);
             var stateName = stateResponse[0].state_name;
             var stateCode = stateResponse[0].state_code;
 
@@ -49,6 +49,7 @@ exports.getData = {
             };
 
             // process district data
+            var districtResponse = flatten(rows[1]);
             var districtName = districtResponse[0].district_name;
             var districtCode = districtResponse[0].district_code;
             final_dict.district = {
@@ -69,29 +70,37 @@ exports.getData = {
                 })
             };
 
-            // process block data
-            var blockName = blockResponse[0].block_name;
-            var blockCode = blockResponse[0].block_code;
-            final_dict.block = {
-                'block_code': blockCode,
-                'block_name': blockName,
-                'data': blockResponse.map(function(d) {
-                    return [
-                        d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
-                        d.mrc_mre,
-                        d.mre_wlg,
-                        d.wlg_wls,
-                        d.wls_fto,
-                        d.fto_sn1,
-                        d.sn1_sn2,
-                        d.sn2_prc,
-                        d.tot_trn
-                    ];
-                })
-            };
+            final_dict.region_name = districtName;
+
+            if (rows.length===3) {
+                // process block data
+                var blockResponse = flatten(rows[2]);
+                var blockName = blockResponse[0].block_name;
+                var blockCode = blockResponse[0].block_code;
+                final_dict.block = {
+                    'block_code': blockCode,
+                    'block_name': blockName,
+                    'data': blockResponse.map(function(d) {
+                        return [
+                            d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
+
+                final_dict.region_name = blockName;
+
+            }
+
 
             var headers = ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'];
-            final_dict.block_name = blockName;
             final_dict.config = {
                 'headers': headers
             };
--- app/controllers/dashboard/performance-panchayat.js
+++ app/controllers/dashboard/performance-panchayat.js
@@ -20,50 +20,30 @@ exports.getData = {
     handler: function(request, reply) {
 
         var sequelize = request.server.plugins.sequelize.db.sequelize;
-        var block_code = request.query.selected_block_id;
-        var queryString = queries.panchayatPerformance(block_code);
+        var region_code = request.query.selected_block_id; // Ravi: update the region code location here
+        var role = 'block'; // Ravi: update the role location here
+        
+        var queryString = queries.panchayatPerformance(block_code,role); // Ravi: update the name of this function
+
 
         sequelize.query(queryString, {
             type: sequelize.QueryTypes.SELECT
         }).then(function(rows) {
 
             var final_dict = {};
-            var blockName = rows[0][0].block_name;
+            var regionName = rows[0][0].region_name;
 
-            var panchayatResponse = flatten(rows[1]);
-            var employeeResponse = flatten(rows[2]);
-
-            var empMapping = {
-                'TA': nestEmpMapping(rows[3]),
-                'GRS': nestEmpMapping(rows[4])
-            }
+            var childrenResponse = flatten(rows[1]);
             
-            var employeeStats = {
-                'past30': {
-                    'TA': nestEmpStats(rows[5]),
-                    'GRS': nestEmpStats(rows[6])
-                },
-                'past60': {
-                    'TA': nestEmpStats(rows[7]),
-                    'GRS': nestEmpStats(rows[8])
-                },
-                'all': {
-                    'TA': nestEmpStats(rows[9]),
-                    'GRS': nestEmpStats(rows[10])
-                }
-            };
-
-            // process panchayat data
-            final_dict.panchayats = d3.nest()
+            // process children data
+            final_dict.children = d3.nest()
                 .key(function(d) {
-                    return d.panchayat_code;
+                    return d.region_code;
                 })
                 .rollup(function(v) {
                     return {
-                        'panchayat_code': v[0].panchayat_code,
-                        'panchayat_name': v[0].panchayat_name,
-                        'mapped_ta': empMapping['TA'][v[0].panchayat_code],
-                        'mapped_grs': empMapping['GRS'][v[0].panchayat_code],
+                        'region_code': v[0].region_code,
+                        'region_name': v[0].region_name,
                         'data': v.map(function(d) {
                             return [
                                 d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
@@ -79,7 +59,7 @@ exports.getData = {
                         })
                     };
                 })
-                .entries(panchayatResponse)
+                .entries(childrenResponse)
                 .map(function(d) {
                     return d.values;
                 })
@@ -91,48 +71,111 @@ exports.getData = {
                     return bSum - aSum;
                 });
 
-            // process employee data
-            final_dict.employees = d3.nest()
-                .key(function(d) {
-                    return d.task_assign;
-                })
-                .rollup(function(v) {
-                    return d3.nest()
-                        .key(function(d) {
-                            return d.staff_id;
-                        })
-                        .rollup(function(w) {
-                            return {
-                                'staff_id': w[0].staff_id,
-                                'name': w[0].name,
-                                'mobile': w[0].mobile_no,
-                                'step1_avg_30':employeeStats['past30'][w[0].task_assign][w[0].staff_id]['step1_avg'],
-                                'tot_trans_30':employeeStats['past30'][w[0].task_assign][w[0].staff_id]['total_transactions'] || 0,
-                                'step1_avg_60':employeeStats['past60'][w[0].task_assign][w[0].staff_id]['step1_avg'],
-                                'tot_trans_60':employeeStats['past60'][w[0].task_assign][w[0].staff_id]['total_transactions'] || 0,
-                                'step1_avg_all':employeeStats['all'][w[0].task_assign][w[0].staff_id]['step1_avg'],
-                                'tot_trans_all':employeeStats['all'][w[0].task_assign][w[0].staff_id]['total_transactions'] || 0,
-                                'panchayats': w.map(function(d) {
-                                    return {
-                                        'panchayat_code': d.map_location,
-                                        'panchayat_name': d.mapped_panchayat_name
-                                    };
-                                })
-                            };
-                        })
-                        .entries(v)
-                        .map(function(d) {
-                            return d.values;
-                        });
-
-                })
-                .map(employeeResponse);
-
             var headers = ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'];
-            final_dict.block_name = blockName;
+            final_dict.region_name = regionName;
             final_dict.config = {
                 'headers': headers,
             };
+
+            if (rows.length>2) { // role==='block'
+
+                var employeeResponse = flatten(rows[2]);
+
+                var empMapping = {
+                    'TA': nestEmpMapping(rows[3]),
+                    'GRS': nestEmpMapping(rows[4])
+                }
+                
+                var employeeStats = {
+                    'past30': {
+                        'TA': nestEmpStats(rows[5]),
+                        'GRS': nestEmpStats(rows[6])
+                    },
+                    'past60': {
+                        'TA': nestEmpStats(rows[7]),
+                        'GRS': nestEmpStats(rows[8])
+                    },
+                    'all': {
+                        'TA': nestEmpStats(rows[9]),
+                        'GRS': nestEmpStats(rows[10])
+                    }
+                };
+
+                // process children data
+                final_dict.children = d3.nest()
+                    .key(function(d) {
+                        return d.region_code;
+                    })
+                    .rollup(function(v) {
+                        return {
+                            'region_code': v[0].region_code,
+                            'region_name': v[0].region_name,
+                            'mapped_ta': empMapping['TA'][v[0].region_code],
+                            'mapped_grs': empMapping['GRS'][v[0].region_code],
+                            'data': v.map(function(d) {
+                                return [
+                                    d.date.getFullYear() + '' + padNum(d.date.getMonth() + 1) + '' + padNum(d.date.getDate()),
+                                    d.mrc_mre,
+                                    d.mre_wlg,
+                                    d.wlg_wls,
+                                    d.wls_fto,
+                                    d.fto_sn1,
+                                    d.sn1_sn2,
+                                    d.sn2_prc,
+                                    d.tot_trn
+                                ];
+                            })
+                        };
+                    })
+                    .entries(childrenResponse)
+                    .map(function(d) {
+                        return d.values;
+                    })
+                    .sort(function(a, b) {
+                        var aTarget = a.data[a.data.length - 1];
+                        var bTarget = b.data[b.data.length - 1];
+                        var aSum = aTarget[1] + aTarget[2] + aTarget[3] + aTarget[4] + aTarget[5] + aTarget[6] + aTarget[7];
+                        var bSum = bTarget[1] + bTarget[2] + bTarget[3] + bTarget[4] + bTarget[5] + bTarget[6] + bTarget[7];
+                        return bSum - aSum;
+                    });
+
+                // process employee data
+                final_dict.employees = d3.nest()
+                    .key(function(d) {
+                        return d.task_assign;
+                    })
+                    .rollup(function(v) {
+                        return d3.nest()
+                            .key(function(d) {
+                                return d.staff_id;
+                            })
+                            .rollup(function(w) {
+                                return {
+                                    'staff_id': w[0].staff_id,
+                                    'name': w[0].name,
+                                    'mobile': w[0].mobile_no,
+                                    'step1_avg_30':employeeStats['past30'][w[0].task_assign][w[0].staff_id]['step1_avg'],
+                                    'tot_trans_30':employeeStats['past30'][w[0].task_assign][w[0].staff_id]['total_transactions'] || 0,
+                                    'step1_avg_60':employeeStats['past60'][w[0].task_assign][w[0].staff_id]['step1_avg'],
+                                    'tot_trans_60':employeeStats['past60'][w[0].task_assign][w[0].staff_id]['total_transactions'] || 0,
+                                    'step1_avg_all':employeeStats['all'][w[0].task_assign][w[0].staff_id]['step1_avg'],
+                                    'tot_trans_all':employeeStats['all'][w[0].task_assign][w[0].staff_id]['total_transactions'] || 0,
+                                    'panchayats': w.map(function(d) {
+                                        return {
+                                            'region_code': d.map_location,
+                                            'region_name': d.mapped_panchayat_name
+                                        };
+                                    })
+                                };
+                            })
+                            .entries(v)
+                            .map(function(d) {
+                                return d.values;
+                            });
+
+                    })
+                    .map(employeeResponse);
+            }
 
             function padNum(num) {
                 var str = num.toString();
@@ -148,24 +191,10 @@ exports.getData = {
                 return array;
             }
 
-            function genMapping(obj) { // generate the lookup object to determine if a panchayat has been mapped
-                var lookup = {
-                    'ta':{},
-                    'grs':{}
-                };
-                obj['ta'].forEach(function(d) {
-                    lookup['ta'][d.panchayat_code] = (d.task_assign === 'TA' ? true : false);
-                });
-                obj['grs'].forEach(function(d) {
-                    lookup['grs'][d.panchayat_code] = (d.task_assign === 'GRS' ? true : false);
-                });
-                return lookup;
-            }
-
             function nestEmpMapping(obj) {
                 return d3.nest()
                     .key(function(d) {
-                        return d.panchayat_code;
+                        return d.region_code;
                     })
                     .rollup(function(v) {
                         return v[0].task_assign === null ? false : true;
