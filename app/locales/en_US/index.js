df0dbacf3d60555ce3347dcaf80d5bdee57b3bc7
Ravi Suhag <suhag.ravi@gmail.com>
2016-02-22 16:10:03 +0530

    Add loading message language

---
+++ app/locales/en_US/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.messages = {
+    not_found: {
+        title: 'Page not found'
+    },
+    loading: 'Please wait, data is loading'
+};
+
+exports.errors = {
+    generic: {
+        title: 'Sorry',
+        subtitle: 'Something went wrong'
+    }
+};
