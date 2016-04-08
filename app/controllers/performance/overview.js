00da07af980f26cb29621235f6e389503c8334ce
edodge <edodge11@gmail.com>
2016-04-07 21:35:59 -0400

    Add monthwise data to web app API

--- app/controllers/performance/overview.js
+++ app/controllers/performance/overview.js
@@ -22,60 +22,83 @@ exports.getData = {
             type: sequelize.QueryTypes.SELECT
         }).then(function(rows) {
             var final_dict = {};
+            final_dict.monthwise = {};
+            final_dict.datewise = {};
 
-            // process state data
-            var stateResponse = utils.flatten(rows[0]);
-            var stateName = stateResponse[0].state_name;
-            var stateCode = stateResponse[0].state_code;
+            if (role==='block') {
+                
+                // process state data
+                var stateResponse = utils.flatten(rows[0]);
+                var stateName = stateResponse[0].state_name;
+                var stateCode = stateResponse[0].state_code;
 
-            final_dict.state = {
-                'state_code': stateCode,
-                'state_name': stateName,
-                'data': stateResponse.map(function(d) {
-                    return [
-                        d.date.getFullYear() + '' + utils.padNum(d.date.getMonth() + 1) + '' + utils.padNum(d.date.getDate()),
-                        d.mrc_mre,
-                        d.mre_wlg,
-                        d.wlg_wls,
-                        d.wls_fto,
-                        d.fto_sn1,
-                        d.sn1_sn2,
-                        d.sn2_prc,
-                        d.tot_trn
-                    ];
-                })
-            };
+                final_dict.monthwise.state = {
+                    'state_code': stateCode,
+                    'state_name': stateName,
+                    'data': stateResponse.map(function(d) {
+                        return [
+                            d.year + '' + utils.padNum(d.month) + '' + utils.padNum(1),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
 
-            // process district data
-            var districtResponse = utils.flatten(rows[1]);
-            var districtName = districtResponse[0].district_name;
-            var districtCode = districtResponse[0].district_code;
-            final_dict.district = {
-                'district_code': districtCode,
-                'district_name': districtName,
-                'data': districtResponse.map(function(d) {
-                    return [
-                        d.date.getFullYear() + '' + utils.padNum(d.date.getMonth() + 1) + '' + utils.padNum(d.date.getDate()),
-                        d.mrc_mre,
-                        d.mre_wlg,
-                        d.wlg_wls,
-                        d.wls_fto,
-                        d.fto_sn1,
-                        d.sn1_sn2,
-                        d.sn2_prc,
-                        d.tot_trn
-                    ];
-                })
-            };
+                // process district data
+                var districtResponse = utils.flatten(rows[1]);
+                var districtName = districtResponse[0].district_name;
+                var districtCode = districtResponse[0].district_code;
+                final_dict.monthwise.district = {
+                    'district_code': districtCode,
+                    'district_name': districtName,
+                    'data': districtResponse.map(function(d) {
+                        return [
+                            d.year + '' + utils.padNum(d.month) + '' + utils.padNum(1),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
 
-            final_dict.region_name = districtName;
-
-            if (rows.length === 3) {
-                // process block data
+                // process block data for bottom chart (grouped by month)
                 var blockResponse = utils.flatten(rows[2]);
                 var blockName = blockResponse[0].block_name;
                 var blockCode = blockResponse[0].block_code;
-                final_dict.block = {
+                final_dict.monthwise.block = {
+                    'block_code': blockCode,
+                    'block_name': blockName,
+                    'data': blockResponse.map(function(d) {
+                        return [
+                            d.year + '' + utils.padNum(d.month) + '' + utils.padNum(1),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
+
+                // process block data for top chart (group by date)
+                var blockResponse = utils.flatten(rows[3]);
+                var blockName = blockResponse[0].block_name;
+                var blockCode = blockResponse[0].block_code;
+                final_dict.datewise.block = {
                     'block_code': blockCode,
                     'block_name': blockName,
                     'data': blockResponse.map(function(d) {
@@ -92,13 +115,88 @@ exports.getData = {
                         ];
                     })
                 };
+
                 final_dict.region_name = blockName;
             }
+            
+
+            if (role==='district') {
+
+                // process state data
+                var stateResponse = utils.flatten(rows[0]);
+                var stateName = stateResponse[0].state_name;
+                var stateCode = stateResponse[0].state_code;
+
+                final_dict.monthwise.state = {
+                    'state_code': stateCode,
+                    'state_name': stateName,
+                    'data': stateResponse.map(function(d) {
+                        return [
+                            d.year + '' + utils.padNum(d.month) + '' + utils.padNum(1),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
+
+                // process district data for bottom chart (monthwise data)
+                var districtResponse = utils.flatten(rows[1]);
+                var districtName = districtResponse[0].district_name;
+                var districtCode = districtResponse[0].district_code;
+                final_dict.top.district = {
+                    'district_code': districtCode,
+                    'district_name': districtName,
+                    'data': districtResponse.map(function(d) {
+                        return [
+                            d.year + '' + utils.padNum(d.month) + '' + utils.padNum(1),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
+
+                // process district data for top chart (datewise data)
+                var districtResponse = utils.flatten(rows[2]);
+                var districtName = districtResponse[0].district_name;
+                var districtCode = districtResponse[0].district_code;
+                final_dict.top.district = {
+                    'district_code': districtCode,
+                    'district_name': districtName,
+                    'data': districtResponse.map(function(d) {
+                        return [
+                            d.date.getFullYear() + '' + utils.padNum(d.date.getMonth() + 1) + '' + utils.padNum(d.date.getDate()),
+                            d.mrc_mre,
+                            d.mre_wlg,
+                            d.wlg_wls,
+                            d.wls_fto,
+                            d.fto_sn1,
+                            d.sn1_sn2,
+                            d.sn2_prc,
+                            d.tot_trn
+                        ];
+                    })
+                };
+
+                final_dict.region_name = districtName;
+            }
+
 
             var headers = ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'];
             final_dict.config = {
                 'headers': headers,
-                'role': request.auth.credentials.role
+                'role': role
             };
 
             reply(final_dict);
