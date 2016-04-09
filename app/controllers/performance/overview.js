fd7adf7261512fc95853fd53cf1c710e65b340b4
Ravi Suhag <suhag.ravi@gmail.com>
2016-04-09 21:54:59 +0530

    Move labels to api for future translation

--- app/controllers/performance/overview.js
+++ app/controllers/performance/overview.js
@@ -34,8 +34,17 @@ exports.getData = {
             }
 
             final_dict.config = {
-                'headers': ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'],
-                'role': role,
+                role: role,
+                headers: ['date', 'mrc_mre', 'mre_wlg', 'wlg_wls', 'wls_fto', 'fto_sn1', 'sn1_sn2', 'sn2_prc', 'tot_trn'],
+                labels: [
+                    'Muster roll closure to muster roll entry',
+                    'Muster roll entry to wage list generation',
+                    'Wage list generation to wage list signing',
+                    'Wage list signing to FTO generation',
+                    'FTO generation to first signature',
+                    'First signature to second signature',
+                    'Second signature to processed by bank',
+                ],
             };
 
             reply(final_dict);
