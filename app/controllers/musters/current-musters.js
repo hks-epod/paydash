da5b3de701206fd42399e230d5709a84d14d60b6
Ravi Suhag <suhag.ravi@gmail.com>
2016-04-11 17:38:33 +0530

    Cleanup delayed and current musters

--- app/controllers/musters/current-musters.js
+++ app/controllers/musters/current-musters.js
@@ -23,28 +23,5 @@ exports.getData = {
         }).then(function(data) {
             reply(data);
         });
-
-        // musters_urls(sequelize, queryString, block_code, function(data) {
-        //     req(urls.musters_on_date, function(error, response, body) {
-        //         if (!error && response.statusCode === 200) {
-        //             var clean_musters = _.uniq(JSON.parse(body), 'muster_no');
-        //             clean_musters.map(function(muster) {
-        //                 muster.start_date = muster.Start_date;
-        //                 delete muster.Start_date;
-        //                 if (urls.employees[muster.panchayat_code + 'GRS']) {
-        //                     muster.grs_name = urls.employees[muster.panchayat_code + 'GRS'].name || 'Unmapped';
-        //                     muster.grs_mobile_no = urls.employees[muster.panchayat_code + 'GRS'].mobile_no || 'Unmapped';
-        //                 } else {
-        //                     muster.grs_name = 'Unmapped';
-        //                     muster.grs_mobile_no = 'Unmapped';
-        //                 }
-        //                 return muster;
-        //             });
-
-        //             reply(clean_musters);
-        //         }
-        //     });
-        // });
-
     }
 };
