08850d56ac5e2f9da17bdbb45292398833ad4b84
Ravi Suhag <suhag.ravi@gmail.com>
2016-07-20 18:30:43 +0530

    Update gulpfile for ES2015

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
 
-var Gulp = require('gulp');
-var RequireDir = require('require-dir');
+const Gulp = require('gulp');
+const RequireDir = require('require-dir');
 
 // Load Tasks
+
 RequireDir('./tasks');
 
+
 //  Build task definition 
+
 Gulp.task('dev-build', ['fonts', 'images', 'misc', 'styles', 'webpack', 'lint']);
 Gulp.task('prod-build', ['dev-build', 'rev']);
 
