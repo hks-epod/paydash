b7589f602aae2be20657eaec5d58a63d126d6d2c
edodge <edodge11@gmail.com>
2016-08-03 21:03:35 +0530

    Update cards API

--- app/controllers/musters/cards.js
+++ app/controllers/musters/cards.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Queries = require('../../helpers/queries');
-const OverviewParser = require('../../helpers/overview_parser');
+const CardsParser = require('../../helpers/cards_parser');
 const Translate = require('../../templates/helpers/t');
 const Utils = require('../../helpers/utils');
 const D3 = require('d3');
@@ -16,29 +16,27 @@ exports.getData = {
     handler: function(request, reply) {
 
         var sequelize = request.server.plugins.sequelize.db.sequelize;
-        var region_code = request.query.region_code;
+        var userId = request.auth.credentials.id;
         var role = request.auth.credentials.role;
-        var queryString = Queries.tiles(region_code, role);
+        var queryString = Queries.cards(userId, role);
 
         sequelize.query(queryString, {
             type: sequelize.QueryTypes.SELECT
         }).then(function(rows) {
 
-            var overviewResponse = Utils.flatten(rows[0]);
-
-            var tilesResponse = Utils.flatten(rows[1]);
-
-            var stateResponse = Utils.flatten(rows[2]);
-
-            // Parse the overview response
-            var current_total = overviewResponse[0].current_total;
-            var delayed_total = overviewResponse[0].delayed_total;
-            var days_to_payment = overviewResponse[0].time_to_payment;
-
-            var state_code = stateResponse[0].state_code;
-
             if (role === 'block') {
-                var tiles = D3.nest()
+
+                var cardsResponse = D3.values(rows[0]);
+
+                var stateResponse = D3.values(rows[1]);
+
+                var state_code = stateResponse[0].state_code;
+
+                // Nest the cards response
+                var cards = D3.nest()
+                    .key(function(d) {
+                        return d.block_code;
+                    })
                     .key(function(d) {
                         return d.staff_id;
                     })
@@ -52,52 +50,53 @@ exports.getData = {
                             'current_total': v[0].current_total,
                             'delayed_total': v[0].delayed_total,
                             'delayed_musters': v.filter(function(d) {
-                                return d.type === 'delayed_musters'; }).map(function(d) {
-                                return [{
+                                return d.type === 'delayed_musters';
+                            }).map(function(d) {
+                                return {
                                     'msr_no': d.msr_no,
                                     'panchayat_name': d.panchayat_name,
                                     'work_name': d.work_name,
                                     'work_code': d.work_code,
                                     'closure_date': d.end_date,
                                     'days_pending': d.days_pending
-                                }];
+                                };
                             }),
                             'current_musters': v.filter(function(d) {
-                                return d.type === 'current_musters'; }).map(function(d) {
-                                return [{
+                                return d.type === 'current_musters';
+                            }).map(function(d) {
+                                return {
                                     'msr_no': d.msr_no,
                                     'panchayat_name': d.panchayat_name,
                                     'work_name': d.work_name,
                                     'work_code': d.work_code,
                                     'closure_date': d.end_date
-                                }];
+                                };
                             })
                         };
                     })
-                    .entries(tilesResponse)
+                    .entries(cardsResponse)
                     .map(function(d) {
-                        return d.values;
+                        return { 
+                            'block_code': d.key, 
+                            'cards': d.value.value
+                        };
+                    })
+                    .map(function(d) {
+                        return d.value;
                     });
+
+                var data = {
+                    'cards': cards
+                };
+
             }
 
             if (role === 'district') {
                 // 
             }
 
-            var final_dict = {
-                'overview': {
-                    'current_total': current_total,
-                    'delayed_total': delayed_total,
-                    'days_to_payment': days_to_payment,
-                    'tiles_total': tiles.length
-                },
-                'tiles': tiles,
-                'config': {
-                    role: role
-                }
-            };
 
-            reply(final_dict);
+            reply(data);
         });
     }
 };
