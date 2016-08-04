e21ac8877b1196bdee7dbb0b500ff696f197fa7e
edodge <edodge11@gmail.com>
2016-08-04 13:06:52 +0530

    Complete district performance queries and parser

--- app/helpers/performance_parser.js
+++ app/helpers/performance_parser.js
@@ -138,13 +138,12 @@ exports.block = function(rows) {
 
 exports.district = function(rows) {
 
-
-    // process state data
-    var stateResponse = Utils.flatten(rows[0]);
+    // Process state data
+    var stateResponse = D3.values(rows[0]);
     var stateName = stateResponse[0].state_name;
     var stateCode = stateResponse[0].state_code;
 
-    final_dict.monthwise.state = {
+    var statePerformance = {
         'state_code': stateCode,
         'state_name': stateName,
         'data': stateResponse.map(function(d) {
@@ -162,50 +161,85 @@ exports.district = function(rows) {
         })
     };
 
-    // Process district data for bottom chart (monthwise data)
-    var districtResponse = Utils.flatten(rows[1]);
-    var districtName = districtResponse[0].district_name;
-    var districtCode = districtResponse[0].district_code;
-    final_dict.monthwise.district = {
-        'district_code': districtCode,
-        'district_name': districtName,
-        'data': districtResponse.map(function(d) {
-            return [
-                d.year + '' + Utils.padNum(d.month) + '' + Utils.padNum(1),
-                d.mrc_mre,
-                d.mre_wlg,
-                d.wlg_wls,
-                d.wls_fto,
-                d.fto_sn1,
-                d.sn1_sn2,
-                d.sn2_prc,
-                d.tot_trn
-            ];
-        })
-    };
+    // Process district data for bottom chart (grouped by month)
+    var districtResponse = D3.values(rows[1]);
 
-    // Process district data for top chart (datewise data)
-    var districtResponse = Utils.flatten(rows[2]);
-    var districtName = districtResponse[0].district_name;
-    var districtCode = districtResponse[0].district_code;
-    final_dict.datewise.district = {
-        'district_code': districtCode,
-        'district_name': districtName,
-        'data': districtResponse.map(function(d) {
-            return [
-                d.date.getFullYear() + '' + Utils.padNum(d.date.getMonth() + 1) + '' + Utils.padNum(d.date.getDate()),
-                d.mrc_mre,
-                d.mre_wlg,
-                d.wlg_wls,
-                d.wls_fto,
-                d.fto_sn1,
-                d.sn1_sn2,
-                d.sn2_prc,
-                d.tot_trn
-            ];
+    // Nest the block response (may be multiple blocks)
+    var districtPerformance = D3.nest()
+        .key(function(d) {
+            return d.district_code;
         })
-    };
+        .rollup(function(v) {
+            return {
+                'district_code': v[0].district_code,
+                'district_name': v[0].district_name,
+                'data': v.map(function(d) {
+                    return [
+                        d.year + '' + Utils.padNum(d.month) + '' + Utils.padNum(1),
+                        d.mrc_mre,
+                        d.mre_wlg,
+                        d.wlg_wls,
+                        d.wls_fto,
+                        d.fto_sn1,
+                        d.sn1_sn2,
+                        d.sn2_prc,
+                        d.tot_trn
+                    ];
+                })
+            };
+        })
+        .entries(districtResponse)
+        .map(function(d) {
+            return d.value;
+        });
 
-    final_dict.region_name = districtName;
-    return final_dict;
+
+    // Process list of block names and codes
+    var blockResponse = D3.values(rows[2]);
+    
+    // Nest the block response
+    var blockPerformance = D3.nest()
+        .key(function(d) {
+            return d.district_code + d.district_name;
+        })
+        .key(function(d) {
+            return d.block_code;
+        })
+        .rollup(function(v) {
+            return {
+                'block_code': v[0].block_code,
+                'block_name': v[0].block_name,
+                'data': v.map(function(d) {
+                    return [
+                        d.year + '' + Utils.padNum(d.month) + '' + Utils.padNum(1),
+                        d.mrc_mre,
+                        d.mre_wlg,
+                        d.wlg_wls,
+                        d.wls_fto,
+                        d.fto_sn1,
+                        d.sn1_sn2,
+                        d.sn2_prc,
+                        d.tot_trn
+                    ];
+                })
+            };
+        })
+        .entries(blockResponse)
+        .map(function(d) {
+            return {
+                'district_code': d.key.substr(0,4),
+                'district_name': d.key.substr(4,),
+                'data': d.values.map(function(e) { 
+                    return e.values;
+                })
+            };
+        });
+
+    var data = {
+        'state_performance': statePerformance,
+        'district_performance': districtPerformance,
+        'block_performance': blockPerformance
+    }
+
+    return data;
 }
