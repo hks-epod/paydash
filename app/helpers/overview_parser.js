356dca7c2655cc90d048720981ba8d8b1797c30d
edodge <edodge11@gmail.com>
2016-08-08 05:57:51 -0400

    Fix issue in overview parser

--- app/helpers/overview_parser.js
+++ app/helpers/overview_parser.js
@@ -5,22 +5,23 @@ const D3 = require('D3');
 exports.parser = function(rows) {
 
     var overviewResponse = D3.values(rows[0]);
+
     var overview = D3.nest()
         .key(function(d) {
             return d.region_code;
         })
         .rollup(function(v) {
             return {
-                'region_code': v[0].region_code,
-                'region_name': v[0].region_name,
-                'current_total': v[0].current_total,
-                'delayed_total': v[0].delayed_total,
-                'days_to_payment': v[0].days_to_payment
+                'region_code': v[0],
+                'region_name': v[1],
+                'current_total': v[2],
+                'delayed_total': v[3],
+                'days_to_payment': v[4]
             }
         })
         .entries(overviewResponse)
         .map(function(d) {
-            return d.values;
+            return d.value;
         });
 
     var data = {
