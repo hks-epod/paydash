9554241803f20894fa81d43d74bccb698d2eb8b6
Ravi Suhag <suhag.ravi@gmail.com>
2016-08-17 15:20:54 +0530

    Add muster numbers

--- assets/scripts/components/card.jsx
+++ assets/scripts/components/card.jsx
@@ -8,7 +8,17 @@ const Card =  React.createClass({
         <div className="pure-u pure-u-6-24">
             <div className="card">
                 <div className="card-head">{this.props.data.name}</div>
-                <div className="card-deisgnation">{this.props.data.designation}</div>
+                <div className="card-designation">{this.props.data.designation}</div>
+                <div className="card-numbers pure-g">
+                    <div className="pure-u pure-u-12-24 ">
+                        <div className="card-number-sub">CURRENT</div>
+                        <div>{this.props.data.current_total}</div>
+                    </div>
+                    <div className="pure-u pure-u-12-24 ">
+                        <div className="card-number-sub">DELAYED</div>
+                        <div>{this.props.data.delayed_total}</div>
+                    </div>
+                </div>
             </div>
         </div>
     ); 
