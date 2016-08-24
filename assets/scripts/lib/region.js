791cec8e1cc85ec17c34b34cc9e8f4eb214cefc3
Ravi Suhag <suhag.ravi@gmail.com>
2016-08-24 09:52:07 +0530

    Add index to region list

--- assets/scripts/lib/region.js
+++ assets/scripts/lib/region.js
@@ -4,19 +4,21 @@ exports.list = function(performance, role) {
     var regions = [];
     if (role === 'block') {
 
-        performance['panchayat'].forEach(function(block){
+        performance['panchayat'].forEach(function(block, index){
             regions.push({
                 value: block.block_code,
                 label: block.block_name,
                 region_type: 'block',
-                class: 'select_parent'
+                class: 'select_parent',
+                index: index
             });     
-            block.data.forEach(function(panchayat){
+            block.data.forEach(function(panchayat, index){
                 regions.push({
                     value: panchayat.panchayat_code,
                     label: panchayat.panchayat_name,
                     region_type: 'panchayat',
-                    class: 'select_child'
+                    class: 'select_child',
+                    index: index
                 });
             });
         });
