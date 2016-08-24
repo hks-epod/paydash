4064cfc12545a6496645d50649adb49cb4881a11
Ravi Suhag <suhag.ravi@gmail.com>
2016-08-25 01:02:20 +0530

    Add verison logic for translations

--- app/templates/helpers/t.js
+++ app/templates/helpers/t.js
@@ -2,20 +2,33 @@
 
 var Confidence = require('confidence');
 var locales = {
-    en_US: require('../../locales/en_US/index'),
-    hi: require('../../locales/hi/index')
+    en_US: {
+        v1: require('../../locales/en_US/v1'),
+        v2: require('../../locales/en_US/v2')
+    },
+    hi: {
+        v1: require('../../locales/hi/v1'),
+        v2: require('../../locales/hi/v2')
+    }
 };
 
-module.exports = function t(path, credentials) {
+module.exports = function t(path, credentials, version) {
 
     var lang = 'hi';
     var criteria = {};
+    var ver;
+
+    if (version) {
+        ver = version;
+    } else {
+        ver = 'v1';
+    }
 
     if (credentials) {
         criteria.role = credentials.role;
         lang = credentials.lang;
     }
 
-    var store = new Confidence.Store(locales[lang]);
+    var store = new Confidence.Store(locales[lang][ver]);
     return store.get(path, criteria);
 };
