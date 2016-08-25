ce49e40f4f9f65ab12aab2c82fb9ef174f2ec197
Ravi Suhag <suhag.ravi@gmail.com>
2016-08-25 13:23:45 +0530

    Apply mouseaction on overview chart

--- assets/scripts/components/performance-chart.jsx
+++ assets/scripts/components/performance-chart.jsx
@@ -11,16 +11,17 @@ const PerformanceChart =  React.createClass({
     loadChart: function(){
 
         var data;
+        var _this = this;
         
-        if(this.props.activeRegion && this.props.activeRegion.region_type === 'block'){
-            data = this.props.performance[this.props.activeRegion.region_type][this.props.activeRegion.block_index].data;
+        if(_this.props.activeRegion && _this.props.activeRegion.region_type === 'block'){
+            data = _this.props.performance[_this.props.activeRegion.region_type][_this.props.activeRegion.block_index].data;
         }
-        else if(this.props.activeRegion && this.props.activeRegion.region_type === 'panchayat'){
-            data = this.props.performance[this.props.activeRegion.region_type][this.props.activeRegion.block_index].data[this.props.activeRegion.panchayat_index].data;
+        else if(_this.props.activeRegion && _this.props.activeRegion.region_type === 'panchayat'){
+            data = _this.props.performance[_this.props.activeRegion.region_type][_this.props.activeRegion.block_index].data[_this.props.activeRegion.panchayat_index].data;
         }else {
             return;
         }
-
+        var legend_target = '.region_legend';
         var parsed_data = Parser.lines({
                 data: data,
                 col: [1, 2, 3, 4, 5, 6, 7],
@@ -29,7 +30,7 @@ const PerformanceChart =  React.createClass({
 
         MG.data_graphic({
                 title: '',
-                target: this.elem,
+                target: _this.elem,
                 left: 100,
                 width: 600,
                 height: 500,
@@ -53,10 +54,41 @@ const PerformanceChart =  React.createClass({
                     label: 'Statutory Limit'
                 }],
                 point_size : 3.5,  
-                legend: this.props.config.labels,
+                legend: _this.props.config.labels,
                 legend_target: '.region_legend',  
-                y_label:  this.props.config.y_axis_label,
-            });
+                y_label:  _this.props.config.y_axis_label,
+                mouseover: function(d, i) {
+                    if (!d.values) {
+                        d.values = [d];
+                        }
+                    if (parsed_data.length) {
+                        for (i = 1; i <= parsed_data.length; i++) {
+                            var l_span = D3.select(legend_target + ' .mg-line' + i + '-legend-color');
+                            l_span.text(' ');
+                            l_span.text('— ' + _this.props.config.labels[i - 1]);
+                        }
+                    }
+                    d.values.forEach(function(val, index) {
+                        var l_span = D3.select(legend_target + ' .mg-line' + val.line_id + '-legend-color');
+                        l_span.text(' ');
+                        var no_days = d.values[index - 1] ? (val.value - d.values[index - 1].value).toFixed(0) : (val.value).toFixed(0);
+                        l_span.text('— ' + _this.props.config.labels[val.line_id - 1] + ' : ' + no_days);
+                        var format = D3.time.format('%b %d, %Y');
+                        D3.select('#region_performance_total_trans').text(format(val.date) + ': ' + val.total_trans);
+                    });
+                },
+                mouseout: function(d, i) {
+                    if (!d.values) {
+                        d.values = [d];
+                    }
+                    D3.select('#region_performance_total_trans').text('');
+                    d.values.forEach(function(val, index) {
+                        var l_span = D3.select(legend_target + ' .mg-line' + val.line_id + '-legend-color');
+                        l_span.text(' ');
+                        l_span.text('— ' + _this.props.config.labels[index]);
+                    });
+                }
+        });
     },
     componentDidMount: function() {
         this.loadChart();
@@ -77,8 +109,7 @@ const PerformanceChart =  React.createClass({
                     <div>Total transactions on <span id="region_performance_total_trans"></span></div>
                 </div>
                 <div className="pure-u-18-24">
-                    <div id="region_performance"></div>
-                    <div ref={el => {if (el){this.elem = el;}}}></div>
+                    <div id="#region_performance" ref={el => {if (el){this.elem = el;}}}></div>
                 </div>
             </div>
         ); 
