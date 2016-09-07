a880306e5286053538177af79fd6b61d33ab9e60
Ravi Suhag <suhag.ravi@gmail.com>
2016-09-07 12:03:51 +0530

    Fix sort by placeholder

--- assets/scripts/components/musters/sort.jsx
+++ assets/scripts/components/musters/sort.jsx
@@ -9,8 +9,8 @@ const Sort = React.createClass({
     render: function(){
         var _this = this;
         return (   
-            <select onChange={this.sort} className="sort-bar button u-pull-right" name="" id="sort-controls">
-                <option value='' disabled="true" defaultValue>Sort by</option>
+            <select onChange={this.sort} className="sort-bar button u-pull-right" defaultValue="0" name="" id="sort-controls">
+                <option value='0' disabled="true">Sort by</option>
                 {
                     _this.props.sortList.map(function(option, i){
                         return (<option key={i} value={option}>{_this.props.translation[option]}</option>);
