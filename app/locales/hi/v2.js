0d8d59d3230dbfb44231eb8d745a0e1b443ea382
Eric Dodge <edodge11@gmail.com>
2016-09-12 11:20:15 -0400

    Add translation for google account

--- app/locales/hi/v2.js
+++ app/locales/hi/v2.js
@@ -99,7 +99,7 @@ module.exports = {
                     connectivity: 'हमे खेद है कि आप log in करने में विफल रहे| कृपया अपने फ़ोन/कंप्यूटर की internet connectivity को जाँच लें|',
                     credentials: 'ऐसा व्यतीत होता है कि आपने ग़लत username या password भरा है | यदि यह समस्या कायम रहती है तो PayDash दल से संपर्क करें|',
                     general: 'हमे खेद है कि आप log in करने में विफल रहे| यदि यह समस्या कायम रहती है तो PayDash दल से संपर्क करें|',
-                    ga_account: 'The Google Account associated with this device does not match the Google Account associated with your PayDash account. Please contact the PayDash team if you require assistance.'
+                    ga_account: 'इस मोबाइल/कंप्यूटर से जुड़ा Google अकाउंट आपके PayDash अकाउंट से जुड़े Google अकाउंट से अलग है| यदि यह समस्या कायम रहती है तो PayDash दल से संपर्क करें|'
                 },
                 logout: {
                     connectivity: 'हमे खेद है कि आप log out करने में विफल रहे| कृपया अपने फ़ोन/कंप्यूटर की internet connectivity को जाँच लें|',
@@ -232,7 +232,7 @@ module.exports = {
                     connectivity: 'हमे खेद है कि आप log in करने में विफल रहे| कृपया अपने फ़ोन/कंप्यूटर की internet connectivity को जाँच लें|',
                     credentials: 'ऐसा व्यतीत होता है कि आपने ग़लत username या password भरा है | यदि यह समस्या कायम रहती है तो PayDash दल से संपर्क करें|',
                     general: 'हमे खेद है कि आप log in करने में विफल रहे| यदि यह समस्या कायम रहती है तो PayDash दल से संपर्क करें|',
-                    ga_account: 'The Google Account associated with this device does not match the Google Account associated with your PayDash account. Please contact the PayDash team if you require assistance.'
+                    ga_account: 'इस मोबाइल/कंप्यूटर से जुड़ा Google अकाउंट आपके PayDash अकाउंट से जुड़े Google अकाउंट से अलग है| यदि यह समस्या कायम रहती है तो PayDash दल से संपर्क करें|'
                 },
                 logout: {
                     connectivity: 'हमे खेद है कि आप log out करने में विफल रहे| कृपया अपने फ़ोन/कंप्यूटर की internet connectivity को जाँच लें|',
