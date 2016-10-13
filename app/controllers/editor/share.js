151ec8657a6cf9380d038ebb45af8ad1ea64b491
Ravi Suhag <suhag.ravi@gmail.com>
2016-10-13 13:21:34 +0530

    Add share controller and email template

--- app/controllers/editor/share.js
+++ app/controllers/editor/share.js
@@ -36,7 +36,39 @@ exports.postRecoveryForm = {
     handler: function(request, reply) {
         //  Create user and send pass info
 
-        
+        Crypto.randomBytes(8, function(err, buffer) {
+
+            var newUser = {
+                email: request.payload.name_email.toLowerCase(),
+                password: Crypto.createHash('md5').update('123456').digest('hex')
+            };
+
+            var User = request.server.plugins.sequelize.db.User;
+
+            User.create(newUser).success(function(user) {
+
+                var data = {
+                    from: 'epodindianrega@gmail.com',
+                    to: user.email,
+                    subject: 'Invitation for data entry - PayDash',
+                    path: 'emails/editor-share',
+                    context: {
+                        name: request.auth.credentials.first_name + request.auth.credentials.last_name,
+                        url: request.connection.info.protocol + '://' + request.info.host + '/login'
+                    }
+                };
+                //  Send Email
+                var Mailer = request.server.plugins.mailer;
+                Mailer.sendMail(data, function(err, info) {
+                    //  Email Sent 
+                });
+                request.yar.flash('success', 'An email has sent to this email id with login instructions');
+                return reply.redirect('/editor/info');
+
+            });
+
+            
+        });
 
     }
 };
