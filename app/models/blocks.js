dbe7750e6373adc3662a3ca282d9b68070bb0b5a
Eric Dodge <edodge11@gmail.com>
2016-11-15 10:04:01 -0500

    UPdate blocks model

--- app/models/blocks.js
+++ app/models/blocks.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-    var Blocks = sequelize.define('blocks', {
+    var Blocks = sequelize.define('Blocks', {
         state_code: DataTypes.STRING,
         state_name: DataTypes.STRING,
         district_code: DataTypes.STRING,
@@ -9,6 +9,7 @@ module.exports = function(sequelize, DataTypes) {
         block_code: DataTypes.STRING,
         block_name: DataTypes.STRING
     }, {
+        tableName: 'blocks',
         timestamps: false,
         underscored: true,
     });
