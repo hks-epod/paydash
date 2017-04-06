65acbb234486f60d83766f77d9ade2134dc244d5
Ravi Suhag <suhag.ravi@gmail.com>
2017-04-06 12:07:05 +0530

    Integrate usage chart api

--- assets/scripts/components/usage/chart.jsx
+++ assets/scripts/components/usage/chart.jsx
@@ -10,19 +10,47 @@ const OverviewChart =  React.createClass({
 
     loadChart: function(){
 
-        console.log(this.props.data);
-
         var _this = this;
+        var data = _this.props.data;
+        var legend_target = '.comparison_legend';
+        var labels = [];
+
+        if(!data.metric) {
+            return;
+        }
+
+        console.log(data.chart_data[0].line_data);
+
         MG.data_graphic({
-            title: 'Singleton',
-            description: 'Handling a solitary data point.',
-            data: [{'date': new Date('2015-03-05T21:00:00Z'), 'value': 12000}],
-            width: 600,
-            height: 200,
-            right: 40,
-            show_tooltips: false,
-            target: _this.elem
-        });
+                title: data.metric_label,
+                target: _this.elem,
+                data: data.chart_data[0].line_data,
+                width: 600,
+                height: 900,
+                left: 100,
+                full_width: true,
+                decimals: 0,
+                xax_count: 10,
+                xax_format: D3.time.format('%b %Y'),
+                chart_type: data.chart_type,
+                missing_text: 'No data',
+                show_secondary_x_label: false,
+                x_extended_ticks: true,
+                legend: labels,
+                legend_target: '.comparison_legend',
+                show_tooltips: false,
+                aggregate_rollover: true,
+                show_year_markers: true,
+                point_size : 3.5,
+                x_accessor:'x_var', 
+                y_accessor:'y_var', 
+                transition_on_update: true,
+                interplate: 'linear',
+                interpolate_tension: 1,
+                area: false,
+                y_label: data.y_label,
+                show_rollover_text: false
+            });
 
     },
     componentDidMount: function() {
@@ -35,7 +63,8 @@ const OverviewChart =  React.createClass({
         return (
             <div className="pure-g">
                 <div className="pure-u-24-24">
-                    <div id="#usage_performance" ref={el => {if (el){this.elem = el;}}}></div>
+                    <div id="usage_performance" ref={el => {if (el){this.elem = el;}}}></div>
+                    <div id="usage_performance_legend"></div>
                 </div>
             </div>
         ); 
