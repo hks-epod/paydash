8c2b17b0b66a03274da29f93e6effd431cee55c4
Ravi Suhag <suhag.ravi@gmail.com>
2017-05-21 00:30:57 +0530

    Add Freshdesk support api

---
+++ app/controllers/api/support.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const Joi = require('joi');
+const FreshDesk = require('../../helpers/freshdesk');
+const Boom = require('boom');
+
+exports.addTicket = {
+    description: 'Add new support ticket',
+    validate: {
+        payload: {
+            subject: Joi.string().max(100).allow(''),
+            email: Joi.string().max(100).allow(''),
+            description: Joi.string().max(100).allow('')
+        },
+        failAction: function(request, reply, source, error) {
+            // Boom bad request
+            return reply.reply(Boom.badRequest(error));
+        }
+    },
+    auth: {
+        mode: 'try',
+        strategy: 'standard'
+    },
+    plugins: {
+        'hapi-auth-cookie': {
+            redirectTo: false
+        }
+    },
+    handler: function(request, reply) {
+        var ticket = {
+            subject: request.payload.subject,
+            email: request.payload.email,
+            description: request.payload.description
+        };
+
+        FreshDesk.newTicket(ticket, function() {
+            return reply({
+                statusCode: 200,
+                message: 'Successfully created ticket.'
+            });
+        });
+    }
+};
