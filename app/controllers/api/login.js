aa25ba6a808d679298129ab917e5153942dcb6dc
Ravi Suhag <suhag.ravi@gmail.com>
2017-08-28 00:09:46 +0530

    Prevent user from loggng out app when logged in from broswer

--- app/controllers/api/login.js
+++ app/controllers/api/login.js
@@ -16,7 +16,8 @@ exports.postForm = {
         }
     },
     validate: {
-        payload: { // payload for POST, query for GET
+        payload: {
+            // payload for POST, query for GET
             username: Joi.string().min(3).max(20),
             password: Joi.string().min(6).max(20),
             google_account: Joi.string().min(6).max(60).optional()
@@ -24,7 +25,7 @@ exports.postForm = {
         failAction: function(request, reply, source, error) {
             // Username, passowrd minimum validation failed
             return reply(Boom.badRequest('Invalid username or password'));
-        },
+        }
     },
     handler: function(request, reply) {
         if (request.auth.isAuthenticated) {
@@ -40,43 +41,34 @@ exports.postForm = {
             // If user account is deactivated
             if (user && user.deactivated) {
                 return reply(Boom.badRequest('User account deactivated'));
-            }
-            
-            // If user is test or pilot 
-            else if (user && (user.type === 'test' || user.type === 'pilot_one')) {
+            } else if (user && (user.type === 'test' || user.type === 'pilot_one')) {
+                // If user is test or pilot
                 request.cookieAuth.set(user);
                 delete user.password;
                 return reply(user);
-            }
-
-            // If user has correct google account info
-            else if (user && user.google_account === request.payload.google_account) {
+            } else if (user && user.google_account === request.payload.google_account) {
+                // If user has correct google account info
                 request.cookieAuth.set(user);
                 delete user.password;
                 return reply(user);
-
-            }
-
-            // If user does not have google account
-            else if (user && !user.google_account) {
-
-                user.update({ google_account: request.payload.google_account }).then(function(user) {
-                    request.cookieAuth.set(user);
-                    delete user.password;
-                    return reply(user);
-                });
-            }
-
-            // If gogole account info provided is not same as user's 
-            else if (user && user.google_account && (user.google_account !== request.payload.google_account)) {
-
+            } else if (user && !user.google_account) {
+                // If user does not have google account
+                user
+                    .update({ google_account: request.payload.google_account })
+                    .then(function(user) {
+                        request.cookieAuth.set(user);
+                        delete user.password;
+                        return reply(user);
+                    });
+            } else if (
+                user &&
+                user.google_account &&
+                user.google_account !== request.payload.google_account
+            ) {
+                // If gogole account info provided is not same as user's
                 return reply(Boom.badRequest('Google account does not match'));
-
-            }
-            
-            // User not fond in database
-            else {
-
+            } else {
+                // User not fond in database
                 return reply(Boom.badRequest('Invalid username or password'));
             }
         });
