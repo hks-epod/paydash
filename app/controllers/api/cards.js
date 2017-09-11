9601d69ce61cee34fd46a6510b67a8a84cf92a65
Eric Dodge <edodge11@gmail.com>
2017-09-11 10:41:20 -0400

    update error logging

--- app/controllers/api/cards.js
+++ app/controllers/api/cards.js
@@ -4,7 +4,6 @@ const Queries = require('../../helpers/queries');
 const Parser = require('../../helpers/paydroid_parser');
 const Boom = require('boom');
 const D3 = require('d3');
-const util = require('util');
 
 exports.getData = {
     auth: {
@@ -43,8 +42,9 @@ exports.getData = {
                 } else if (version === 2) {
 
                     if (D3.values(rows[4])[0]===undefined) {
-                        console.log("Couldn't find state code in db for request:");
-                        console.log(util.inspect(request, {showHidden: false, depth: null}));
+                        console.log("Couldn't find state code in db for id:");
+                        // console.log(util.inspect(request, {showHidden: false, depth: null}));
+                        console.log(request.auth.credentials.id);
                         return reply(Boom.badRequest('Bad request'));
                     }
 
